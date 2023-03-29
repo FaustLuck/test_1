76942c54fafe1a6d768653a52041d3e078219a7c
faustluck <faustluck91@gmail.com>
2023-03-29 03:01:34 +0300

    toggle menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,48 +1,74 @@
 const searchButton = document.querySelector(".search");
 const catalogButton = document.querySelector(".catalog");
 const menuButton = document.querySelector(".button_menu-mobile");
+const menu = document.querySelector(".menu");
 const closeButton = document.querySelector(".button-close");
 
 
 function openSearch() {
   if (!window.matchMedia("(max-width: 576px)").matches) return;
-  searchButton.classList.add("open");
-  toggleOverlay();
+  toggleOverlay(searchButton);
   const {bottom} = searchButton.getBoundingClientRect();
   closeButton.style.top = `${bottom + 20}px`;
   closeButton.style.left = "50%";
   closeButton.style.transform = "translateX(-50%)";
-  closeButton.classList.remove("hide");
-
   searchButton.removeEventListener("pointerdown", openSearch);
   closeButton.addEventListener("pointerdown", closeSearch);
 }
 
 function closeSearch() {
-  searchButton.classList.remove("open");
-  toggleOverlay();
-  closeButton.classList.add("hide");
+  toggleOverlay(searchButton);
   closeButton.style = "";
-
   searchButton.addEventListener("pointerdown", openSearch);
   closeButton.removeEventListener("pointerdown", closeSearch);
 }
 
 function openCatalog() {
   if (!window.matchMedia("(max-width: 576px)").matches) return;
-  console.log(1);
+  toggleOverlay(catalogButton);
+  const {left, top} = menuButton.getBoundingClientRect();
+  closeButton.style.left = left + "px";
+  closeButton.style.top = top + "px";
+  catalogButton.removeEventListener("pointerdown", openCatalog);
+  closeButton.addEventListener("pointerdown", closeCatalog);
 }
 
 function closeCatalog() {
+  toggleOverlay(searchButton);
+  closeButton.style = "";
+  catalogButton.addEventListener("pointerdown", openCatalog);
+  closeButton.removeEventListener("pointerdown", closeCatalog);
+}
+
+function openMenu() {
+  if (!window.matchMedia("(max-width: 576px)").matches) return;
+  toggleOverlay(menuButton);
+  const {left, top} = menuButton.getBoundingClientRect();
+  closeButton.style.left = left + "px";
+  closeButton.style.top = top + "px";
+  menu.classList.remove("hide");
+  menuButton.removeEventListener("pointerdown", openMenu);
+  closeButton.addEventListener("pointerdown", closeMenu);
+}
+
+function closeMenu() {
+  toggleOverlay(menuButton);
+  menu.classList.add("hide");
+  closeButton.style = "";
+  menuButton.addEventListener("pointerdown", openMenu);
+  closeButton.removeEventListener("pointerdown", closeMenu);
 }
 
 searchButton.addEventListener("pointerdown", openSearch);
 catalogButton.addEventListener("pointerdown", openCatalog);
+menuButton.addEventListener("pointerdown", openMenu);
 
-function toggleOverlay() {
+function toggleOverlay(target) {
   const overlay = document.querySelector(".overlay");
   const body = document.body;
   const isClose = !overlay.classList.contains("open");
+  target.classList.toggle("open", isClose);
   overlay.classList.toggle("open", isClose);
   body.classList.toggle("open", isClose);
+  closeButton.classList.toggle("hide", !isClose);
 }

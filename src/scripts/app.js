df8de1b9bc215f41e567c9dba7170f802756572d
faustluck <faustluck91@gmail.com>
2023-03-29 13:53:09 +0300

    js refactor + add plug

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,13 +1,15 @@
-const searchButton = document.querySelector(".search");
-const catalogButton = document.querySelector(".catalog");
-const menuButton = document.querySelector(".button_menu-mobile");
-const menu = document.querySelector(".menu");
-const closeButton = document.querySelector(".button-close");
+const searchButton = document.querySelector(".search-js");
+const catalogButton = document.querySelector(".catalog-js");
+const catalogMenu = document.querySelector(".menu-catalog-js");
+const menuButton = document.querySelector(".button_menu-js");
+const menu = document.querySelector(".sidebar-js");
+const closeButton = document.querySelector(".button-close-js");
 
 
 function openSearch() {
   if (!window.matchMedia("(max-width: 576px)").matches) return;
-  toggleOverlay(searchButton);
+  insertPlug(searchButton);
+  toggleOverlay([searchButton]);
   const {bottom} = searchButton.getBoundingClientRect();
   closeButton.style.top = `${bottom + 20}px`;
   closeButton.style.left = "50%";
@@ -17,7 +19,8 @@ function openSearch() {
 }
 
 function closeSearch() {
-  toggleOverlay(searchButton);
+  toggleOverlay([searchButton]);
+  removePlug();
   closeButton.style = "";
   searchButton.addEventListener("pointerdown", openSearch);
   closeButton.removeEventListener("pointerdown", closeSearch);
@@ -25,24 +28,28 @@ function closeSearch() {
 
 function openCatalog() {
   if (!window.matchMedia("(max-width: 576px)").matches) return;
-  toggleOverlay(catalogButton);
+  insertPlug(catalogButton);
+  toggleOverlay([catalogButton, catalogMenu]);
   const {left, top} = menuButton.getBoundingClientRect();
   closeButton.style.left = left + "px";
   closeButton.style.top = top + "px";
   catalogButton.removeEventListener("pointerdown", openCatalog);
   closeButton.addEventListener("pointerdown", closeCatalog);
+  catalogButton.addEventListener("pointerdown", closeCatalog);
 }
 
 function closeCatalog() {
-  toggleOverlay(searchButton);
+  toggleOverlay([catalogButton, catalogMenu]);
+  removePlug();
   closeButton.style = "";
   catalogButton.addEventListener("pointerdown", openCatalog);
+  catalogButton.removeEventListener("pointerdown", closeCatalog);
   closeButton.removeEventListener("pointerdown", closeCatalog);
 }
 
 function openMenu() {
   if (!window.matchMedia("(max-width: 576px)").matches) return;
-  toggleOverlay(menuButton);
+  toggleOverlay([menuButton]);
   const {left, top} = menuButton.getBoundingClientRect();
   closeButton.style.left = left + "px";
   closeButton.style.top = top + "px";
@@ -52,7 +59,7 @@ function openMenu() {
 }
 
 function closeMenu() {
-  toggleOverlay(menuButton);
+  toggleOverlay([menuButton]);
   menu.classList.add("hide");
   closeButton.style = "";
   menuButton.addEventListener("pointerdown", openMenu);
@@ -63,12 +70,26 @@ searchButton.addEventListener("pointerdown", openSearch);
 catalogButton.addEventListener("pointerdown", openCatalog);
 menuButton.addEventListener("pointerdown", openMenu);
 
-function toggleOverlay(target) {
+function toggleOverlay(targets) {
   const overlay = document.querySelector(".overlay");
   const body = document.body;
   const isClose = !overlay.classList.contains("open");
-  target.classList.toggle("open", isClose);
+  targets.forEach(target => {
+    target.classList.toggle("open", isClose);
+  });
   overlay.classList.toggle("open", isClose);
   body.classList.toggle("open", isClose);
   closeButton.classList.toggle("hide", !isClose);
+}
+
+function insertPlug(target) {
+  const plug = target.cloneNode(true);
+  plug.classList.remove("catalog-js");
+  plug.classList.add("plug-js");
+  target.parentNode.insertBefore(plug, target);
+}
+
+function removePlug() {
+  const plug = document.querySelector(".plug-js");
+  plug.remove();
 }
